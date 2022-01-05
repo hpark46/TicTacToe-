ccaf01489efce60b1fb517b711bbe41fe68c99f9
hpark46 <36461411+hpark46@users.noreply.github.com>
2022-01-05 19:36:06 +0900

    framework for running tic_tac_toe with minimax a.i.

---
+++ ttt2.0/run_tic_tac_toe.py
@@ -0,0 +1,63 @@
+import math
+import sys
+from state_space import *
+from Minimax import *
+
+
+
+# def run():
+# 	# print("please choose the algorithm to play against")
+# 	# mode = input("a: minimax,  b: h-minimax")
+# 	# if (mode == "a"):
+
+
+# 	player = input("please choose o or x")
+# 	print("x goes first")
+
+
+
+# 	board, turn, player = initialize(player)
+
+
+# 	if (player == 'x'):
+
+
+
+
+
+# 	else:
+
+
+
+
+
+
+def translate_input(cell_number):
+	
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	print(translate_input('1'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
